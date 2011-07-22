3dfca3d17e0af646bacc08b6b77c6af2b949a7ad
Matt DeBoard <matt.deboard@gmail.com>
2011-07-21 19:09:32 -0900

    Rolled in some changes to urlencoding & hashing...

--- python_usdol.py
+++ python_usdol.py
@@ -1,39 +1,24 @@
-import base64
 import datetime
 import hashlib
 import hmac
-import httplib2
-import lxml
-import re
-import sys
-import urllib
 import urllib2
+import urlparse
 
 from usdol_secret import API_AUTH_KEY, API_SHARED_SECRET
 
 USDOL_URL = 'http://api.dol.gov'
-HEADER_PREFIX = '/V1/FORMS/Agencies?$top=10'
-HEADER_TIMESTAMP = 'Timestamp='
-HEADER_API_PREFIX = '&ApiKey='
-HEADER_SIG_PREFIX = '&Signature='
+
 
 class Connection(object):
     token = API_AUTH_KEY
     secret = API_SHARED_SECRET
     
-    def __init__(self, *args, **kwargs):
-        hdr_value = self._get_signature()
-        print >> sys.stdout, hdr_value
-        self.headers = {'Authorization': urllib.urlencode(hdr_value),
-                        'Accept': 'application/json'}
-        self.auth_url = USDOL_URL + HEADER_PREFIX
+    def __init__(self, baseurl='/V1/FORMS/Agencies?top=10'):
+        self.baseurl = baseurl
 
-    def get_data(self):
-        http = httplib2.Http()
-        url = self.auth_url
-        print >> sys.stdout, url
-        r, c = http.request(url, 'GET', headers=self.headers)
-        return (r, c)
+    def _urlencode(self, d):
+        ret = ['%s=%s' % (k, v) for k, v in d.iteritems()]
+        return '&'.join(ret)
         
     def _get_timestamp(self):
         '''
@@ -42,21 +27,30 @@ class Connection(object):
         There is a 15-minute window for an auth string with a valid
         timestamp to be accepted by the US DOL's servers.
         '''
-        t = datetime.datetime.now().isoformat()
-        return t.split('.')[0] + 'Z'
+        t = datetime.datetime.utcnow().replace(microsecond=0)
+        return (t, t.isoformat()+'Z')
 
-    def _get_apikey(self):
-        return API_AUTH_KEY
+    def _get_message(self):
+        date_time, timestamp = self._get_timestamp()
+        header_dict = {"Timestamp": timestamp, "ApiKey": self.token}
+        return (header_dict, '%s&%s' % (self.baseurl,
+                                        self._urlencode(header_dict)))
 
-    def _get_signature(self):
-        m = dict(Timestamp=self._get_timestamp(),
-                 ApiKey=self._get_apikey())
-        digest = hmac.new(self.secret,
-                          msg=HEADER_PREFIX+urllib.urlencode(m),
-                          digestmod=hashlib.sha1).digest()
-        out = base64.b64encode(digest).decode()
-        m.update(dict(Signature=out))
-        return m
+    def _get_header(self):
+        d, message = self._get_message()
+        h = hmac.new(self.secret, message, hashlib.sha1)
+        d['Signature'] = h.hexdigest()
+        return self._urlencode(d)
+
+    def _get_request(self):
+        header = self._get_header()
+        qs = self.baseurl
+        url = urlparse.urljoin(USDOL_URL, qs)
+        req = urllib2.Request(url, headers={"Authorization": header})
+        return req
+
+    def get_data(self):
+        return urllib2.urlopen(self._get_request())
                         
         
         
