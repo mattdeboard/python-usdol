c3c9f6714c9720b4b7ec4c936c7671832c72d141
Matt DeBoard <matt.deboard@gmail.com>
2011-07-22 17:17:00 -0900

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(name="Python-USDOL",
+      version="1.0",
+      description="Python module to access US Dept. of Labor's dataset API.",
+      author="Matt DeBoard",
+      author_email="mattdeboard@gmail.com",
+      py_modules=["usdol"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Legal Industry',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Topic :: Education',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Utilities'])
+          
+      
